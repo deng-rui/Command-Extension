83ddd7d3f40f9dc79f2f25baafb15c74e0ed01bb
deng-rui <deng-rui@foxmail.com>
2020-01-30 11:09:20 +0800

    update

---
+++ src/main/java/auxiliary/Booleans.java
@@ -0,0 +1,15 @@
+package extension.auxiliary;
+
+
+public class Booleans {
+	private static boolean vote = true;
+//初始化
+	public static boolean getvote() {
+		return vote;
+	}
+//读取
+	public static void setvote(boolean vote){
+		Booleans.vote = vote;
+	}
+//设定
+}
---
+++ src/main/java/auxiliary/Strings.java
@@ -0,0 +1,5 @@
+package extension.auxiliary;
+
+
+public class Strings {
+}
