1550edb567a330eb30e528d550eaee027e10eb5e
deng-rui <deng-rui@foxmail.com>
2020-02-01 10:10:29 +0800

    UPDATE DEATUTIL

--- src/main/java/tool/DateUtil.java
+++ src/main/java/tool/DateUtil.java
@@ -1,46 +1,47 @@
 package extension.tool;
 
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
-import java.util.Date;
+
 
 public class DateUtil {
-
-    public static String timeStamp2Date(String seconds) {  
-        if(seconds == null || seconds.isEmpty() || seconds.equals("null")){  
-            return "";  
-        }  
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-        return sdf.format(new Date(Long.valueOf(seconds+"000")));  
-    }  
-
-    public static String date2TimeStamp(String date_str){  
-        try {  
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-            return String.valueOf(sdf.parse(date_str).getTime()/1000);  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-        return "";  
-    }  
-
-    public static String timeStamp(){  
-        long time = System.currentTimeMillis();
-        String t = String.valueOf(time/1000);  
-        return t;  
-    }  
-
-    public static boolean iftime(int a,int b) throws ParseException
-    {
-        //如果想比较日期则写成"yyyy-MM-dd"就可以了
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        //将字符串形式的时间转化为Date类型的时间
-        //Date类的一个方法，如果a早于b返回true，否则返回false
-        if(a-b<0) {
-            System.out.println("A");return true;
-        }else{
-            System.out.println("BDtime");return false;
-}
-    }
-
+	public static String getUTCTimeStr() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		 
+		StringBuffer UTCTimeBuffer = new StringBuffer();
+		Calendar cal = Calendar.getInstance();
+		int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
+		int dstOffset = cal.get(Calendar.DST_OFFSET);
+		cal.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		UTCTimeBuffer.append(year).append("-").append(month).append("-").append(day);
+		UTCTimeBuffer.append(" ").append(hour).append(":").append(minute).append(":").append(second );
+		try {
+			format.parse(UTCTimeBuffer.toString());
+			return UTCTimeBuffer.toString();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		 
+		return null;
+	}
+ 
+	   public static String getLocalTimeFromUTC(String UTCTime, String ){
+		Date UTCDate;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		 
+		String localTimeStr = null ;
+		try {
+			UTCDate = format.parse(UTCTime);
+			format.setTimeZone(TimeZone.getTimeZone("GMT-8")) ;
+			localTimeStr = format.format(UTCDate) ;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		 
+		return localTimeStr ;
+	}
 }
