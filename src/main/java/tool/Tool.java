6ab18c70bdc625de57cd0efa38b47af4ba9e2996
deng-rui <deng-rui@foxmail.com>
2020-02-18 09:03:50 +0800

    remove test-1

--- src/main/java/tool/Tool.java
+++ src/main/java/tool/Tool.java
@@ -34,11 +34,8 @@ public class Tool {
 	}
 	
 	public static boolean Authority_control(String uuid, String a) {
-		System.out.println("?:");
-		System.out.println(String.valueOf(getSQLite(uuid).get(19)));
 		if(!Player_power_Date_boolean(uuid))setPlayer_power_Date(uuid,Integer.valueOf(String.valueOf(getSQLite(uuid).get(19))).intValue());
 		if(!Power_Date_boolean(getPlayer_power_Date(uuid)))return false;
-		//System.out.println(getPower_Date(getPlayer_power_Date(uuid)).contains(a));
 		return (boolean)getPower_Date(getPlayer_power_Date(uuid)).contains(a);
 	}
 	//算法写的烂 bug满天飞
