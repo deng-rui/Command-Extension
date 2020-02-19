dbd9c1fb3f360f80521c2a6823671176385e843b
deng-rui <deng-rui@foxmail.com>
2020-02-19 20:48:48 +0800

    UPDATE: Update country time GMT

--- src/main/java/tool/DateUtil.java
+++ src/main/java/tool/DateUtil.java
@@ -8,45 +8,24 @@ import java.text.ParseException;
 
 
 public class DateUtil {
-	public static String getUTCTimeStr() {
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		 
-		StringBuffer UTCTimeBuffer = new StringBuffer();
-		Calendar cal = Calendar.getInstance();
-		int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
-		int dstOffset = cal.get(Calendar.DST_OFFSET);
+	public static long getUTCTimeStr() {
+		Calendar cal = Calendar.getInstance(); 
+		//2、取得时间偏移量：  
+		final int zoneOffset = cal.get(Calendar.ZONE_OFFSET); 
+		//3、取得夏令时差：  
+		final int dstOffset = cal.get(Calendar.DST_OFFSET);  
+		//4、从本地时间里扣除这些差量，即可以取得UTC时间：  
 		cal.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
-		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH) + 1;
-		int day = cal.get(Calendar.DAY_OF_MONTH);
-		int hour = cal.get(Calendar.HOUR_OF_DAY);
-		int minute = cal.get(Calendar.MINUTE);
-		int second = cal.get(Calendar.SECOND);
-		UTCTimeBuffer.append(year).append("-").append(month).append("-").append(day);
-		UTCTimeBuffer.append(" ").append(hour).append(":").append(minute).append(":").append(second );
-		try {
-			format.parse(UTCTimeBuffer.toString());
-			return UTCTimeBuffer.toString();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		 
-		return null;
+		return cal.getTimeInMillis();
 	}
  
-	   public static String getLocalTimeFromUTC(String UTCTime, String GMT){
-		Date UTCDate;
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		 
-		String localTimeStr = null ;
-		try {
-			UTCDate = format.parse(UTCTime);
-			format.setTimeZone(TimeZone.getTimeZone(GMT)) ;
-			localTimeStr = format.format(UTCDate) ;
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		 
-		return localTimeStr ;
+	public static String getLocalTimeFromUTC(long GMT, int fot){
+		String[] ft=new String[]{"yyyy-MM-dd'T'HH:mm:ss'Z'","yyyy-MM-dd HH:mm:ss","dd-MM-yyyy HH:mm:ss","MM-dd-yyyy HH:mm:ss"};
+		long UTC = getUTCTimeStr();
+		System.out.println(UTC);
+		UTC = UTC + GMT;
+		SimpleDateFormat sdf=new SimpleDateFormat(ft[fot]);
+		long date_temp = Long.valueOf(UTC/1000);    
+		return sdf.format(new Date(date_temp * 1000L));
 	}
 }
