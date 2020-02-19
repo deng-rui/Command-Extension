578d51d785d5189d156571b6ede57eae29671c9d
deng-rui <deng-rui@foxmail.com>
2020-02-19 20:56:04 +0800

    update: Temporary storage of player data--forshadowing

--- src/main/java/auxiliary/Lists.java
+++ src/main/java/auxiliary/Lists.java
@@ -5,29 +5,38 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lists {
-	/*
-	private static List<String> Tourist = Collections.synchronizedList(new ArrayList<String>());
+	//private static List<String> PlayerDate_SQL_Temp = Collections.synchronizedList(new ArrayList<String>());
 
-	public static void addTourist(String uuid) {
-		Tourist.add(uuid);
+	//public static void addPlayerDate_SQL_Temp(List list) {
+	//	PlayerDate_SQL_Temp = list;
+	//}
+	//
+
+	public static List updatePlayerDate_SQL_Temp(List list, String old, String nw) {
+		List<String> tempList = new ArrayList<String>(list.size()+1);  
+		for(int i=0;i<list.size();i++){  
+			if(!list.get(i).equals(old)){  
+				tempList.add((String)list.get(i));  
+			} else {
+				tempList.add(nw);
+			}
+		}  
+		return tempList;  
 	}
-	//
 
-	public static void removeTourist(String str) {    
-         List<String> tempList = new ArrayList<String>(Tourist.size());  
-         for(int i=0;i<Tourist.size();i++){  
-             if(!Tourist.get(i).equals(str)){  
-                 tempList.add(Tourist.get(i));  
-             }  
-         }  
-         Tourist = tempList;  
-    }
-	//
+	/*
+	public static List updatePlayerDate_SQL_Temp() {
+		return PlayerDate_SQL_Temp
+	}
+
+	public static String updatePlayerDate_SQL_Temp(int i) {
+		return PlayerDate_SQL_Temp.get(i);
+	}
+	
 
 	public static boolean getplayer_boolean(String uuid) {
 		return Tourist.contains(uuid);
 	}
 	//是否存在
 	*/
-	//disuse/弃用
 }
--- src/main/java/auxiliary/Maps.java
+++ src/main/java/auxiliary/Maps.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Maps {
 	private static Map<String, Integer> Player_Sensitive_words = Collections.synchronizedMap(new HashMap<String, Integer>());
 	private static Map<String, String> PlayerDate_Temp = Collections.synchronizedMap(new HashMap<String, String>());
+	private static Map<String, List> PlayerDate_SQL_Temp = Collections.synchronizedMap(new HashMap<String, List>());
 	private static Map<String, Integer> Player_power_Date = Collections.synchronizedMap(new HashMap<String, Integer>());
 	private static Map<Integer, List> Power_Date = Collections.synchronizedMap(new HashMap<Integer, List>());
 	//Safety! Inefficient :(
@@ -28,6 +29,10 @@ public class Maps {
 	public static List getPower_Date(int id) {
 		return Power_Date.get(id);
 	}
+
+	public static List getPlayerDate_SQL_Temp(String uuid) {
+		return PlayerDate_SQL_Temp.get(uuid);
+	}
 	//读取
 
 	public static void setPlayer_Sensitive_words(String key, int i) {
@@ -45,7 +50,21 @@ public class Maps {
 	public static void setPower_Date(int id, List list) {
 		Power_Date.put(id, list);
 	}
+
+	public static void setPlayerDate_SQL_Temp(String uuid, List list) {
+		PlayerDate_SQL_Temp.put(uuid, list);
+	}
 	//设置
+
+	public static void removePlayerDate_Temp(String uuid, String key) {
+		PlayerDate_Temp.remove(uuid+key);
+	}
+
+	public static void removePlayerDate_SQL_Temp(String uuid) {
+		PlayerDate_SQL_Temp.remove(uuid);
+	}
+
+	//删除
 
 	public static boolean Player_Sensitive_words_boolean(String key) {
 		return Player_Sensitive_words.containsKey(key);
