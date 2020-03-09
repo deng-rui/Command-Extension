c6d2565f0ff68e293cc55cc97808dd335ca33432
deng-rui <deng-rui@foxmail.com>
2020-03-09 13:51:39 +0800

    update: config.java

---
+++ src/main/java/data/global/Config.java
@@ -0,0 +1,10 @@
+package extension.data.global;
+
+public class Config {
+
+	public static String Plugin_Path 				= "config/mods";
+	public static String Plugin_Data_Path 			= "config/mods/GA";
+	public static String Plugin_Lib_Path 			= "config/mods/GA/lib";
+	public static String Plugin_Resources_Path 		= "config/mods/GA/resources";
+
+}
