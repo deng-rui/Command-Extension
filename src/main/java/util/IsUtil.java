61ea97e068052e958ec17767d60d0caedb14080a
deng-rui <deng-rui@foxmail.com>
2020-04-01 18:32:16 +0800

    Change file name change type

---
+++ src/main/java/util/IsUtil.java
@@ -0,0 +1,24 @@
+package extension.util;
+
+import java.util.regex.Pattern;
+
+public class IsUtil {
+	
+	public static boolean Blank(Object string) {
+		if (string == null || "".equals(string.toString().trim())) return true;
+		return false;
+	}
+
+	public static boolean NotBlank(Object string) {
+		return !Blank(string);
+	}
+
+	public static boolean isNumeric(String string) {
+		final Pattern pattern = Pattern.compile("[0-9]*");
+		return pattern.matcher(string).matches();
+	}
+
+	public static boolean NotisNumeric(String string) {
+		return !isNumeric(string);
+	}
+}
