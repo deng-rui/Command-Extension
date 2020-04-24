63d11d66aab346c095a974631fa2a15253f9e94e
deng-rui <deng-rui@foxmail.com>
2020-04-24 18:32:53 +0800

    Add a new column, please use a user migration

--- src/main/java/data/db/SQLite.java
+++ src/main/java/data/db/SQLite.java
@@ -27,53 +27,62 @@ public class SQLite {
 			Statement stmt = c.createStatement();
 			// 时间可以改成BUGINT
 			sql = "CREATE TABLE PlayerData (" +
-				  "UUID 			TEXT,"+
-				  "User 			TEXT,"+
-				  "IP 				BIGINT(4),"+
-				  "GMT 				INT,"+
-				  "Country 			TEXT,"+
-				  "Time_format 		TINYINT(1),"+
-				  "Language 		CHAR(5),"+
-				  "LastLogin 		BIGINT,"+
+				  "UUID 					TEXT,"+
+				  "User 					TEXT,"+
+				  "IP 						BIGINT(4),"+
+				  "GMT 						INT,"+
+				  "Country 					TEXT,"+
+				  "Time_format 				TINYINT(1),"+
+				  "Language 				CHAR(5),"+
+				  "LastLogin 				BIGINT,"+
 				  //
-				  "Buildcount 		INTEGER,"+
-				  "Dismantledcount 	INTEGER,"+
-				  "Cumulative_build INTEGER,"+
-				  "Pipe_build 		INTEGER,"+
+				  "Buildcount 				INTEGER,"+
+				  "Dismantledcount 			INTEGER,"+
+				  "Cumulative_build 		INTEGER,"+
+				  "Pipe_build 				INTEGER,"+
 				  //玩家普通信息
-				  "Kickcount		INTEGER,"+
+				  "Kickcount				INTEGER,"+
 				  //被踢次数
-				  "Translate 		TINYINT(1),"+
+				  "Translate 				TINYINT(1),"+
 				  //翻译权限
-				  "Level 			INTEGER,"+
-				  "Exp 				BIGINT,"+
-				  "Reqexp 			BIGINT,"+
-				  "Reqtotalexp 		BIGINT,"+
+				  "Level 					INTEGER,"+
+				  "Exp 						BIGINT,"+
+				  "Reqexp 					BIGINT,"+
+				  "Reqtotalexp 				BIGINT,"+
 				  //等级
-				  "Playtime 		BIGINT,"+
+				  "Playtime 				BIGINT,"+
 				  //游戏时长
-				  "Pvpwincount 		INTEGER,"+
-				  "Pvplosecount 	INTEGER,"+
+				  "Pvpwincount 				INTEGER,"+
+				  "Pvplosecount 			INTEGER,"+
 				  //胜利 输数
-				  "Authority 		TINYINT(1),"+
+				  "Authority 				TINYINT(1),"+
+				  "Authority_effective_time BIGINT,"+
 				  //权限1
-				  "Lastchat 		BIGINT,"+
+				  "Lastchat 				BIGINT,"+
 				  //最后聊天时间 聊天计数
-				  "Deadcount		INTEGER,"+
-				  "Killcount 		INTEGER,"+
-				  "Joincount 		INTEGER,"+
-				  "Breakcount 		INTEGER)"; 
+				  "Deadcount				INTEGER,"+
+				  "Killcount 				INTEGER,"+
+				  "Joincount 				INTEGER,"+
+				  "Breakcount 				INTEGER)"; 
 				  //玩家死亡 击杀 加入 退出次数
 				  //TEST阶段 仅在GA-PVP使用
 			stmt.executeUpdate(sql);
 			sql = "CREATE TABLE PlayerPrivate (" +
-				  "User 			TEXT,"+
-				  "Mail 			CHAR(50),"+
-				  "Online 			TINYINT(1),"+
-				  "PasswordHash 	TEXT,"+
-				  "CSPRNG 			TEXT)";
+				  "User 					TEXT,"+
+				  "Mail 					CHAR(50),"+
+				  "Online 					TINYINT(1),"+
+				  "PasswordHash 			TEXT,"+
+				  "CSPRNG 					TEXT)";
 			//Cryptographically Secure Pseudo-Random Number Generator
 			//安全系列3
+			stmt.executeUpdate(sql);
+			sql = "CREATE TABLE KeyData (" +
+				  "KEY 						TEXT,"+
+				  "Authority 				TINYINT(1),"+
+				  "Total 					TINYINT,"+
+				  "Surplus 					TINYINT,"+
+				  "Time 					BIGINT,"+
+				  "Expire 					BIGINT)";
 			stmt.executeUpdate(sql);
 			stmt.close();
 			c.close();
